a272fba6a1971537d47db34daa1ff65a09d3b679
Alberto Turiel <alberto.turiel@ng-branch-technology.com>
2019-12-17 16:40:51 +0100

    Config for my setup with BME-BAT and no LED

--- nodemcu/lua/config.lua
+++ nodemcu/lua/config.lua
@@ -1,23 +1,20 @@
 local module = {}
 
--- Make your adjustments below here
-module.Model = "BME"
+-- Make your adjustments below here: DHT, DHT-YL, BME, BME-GD, BME-BAT
+module.Model = "BME-BAT"
 module.Version = "1.6"
 
 -- LED state
-module.ledState = 1 -- 0: fully disabled, 1: LEDs on, 2: Connected off (Boot/Error only)
+module.ledState = 0 -- 0: fully disabled, 1: LEDs on, 2: Connected off (Boot/Error only)
 
 -- BME280 settings
-
 module.bme280scl = 5  -- D5
 module.bme280sda = 6  -- D6
 
 -- DHT22 settings
-
 module.DHT22 = 2
 
 -- YL69 Moisture Sensor
-
 module.YL69 = 0 -- adc pin 0
 module.YL69Power = 7 -- D7
 
